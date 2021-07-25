15a50dcca00060ca633debaf5d6e12a40f0a3bda
Gustavo Bomfim <gustavobomfim@alunos.utfpr.edu.br>
2021-07-25 17:29:58 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -9,16 +9,24 @@ namespace xadrez
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                    partida.executaMovimento(origem, destino);
+                }
 
-                Tela.imprimirTabuleiro(tab);
+                
+               
             }
             catch(TabuleiroException e)
             {
--- xadrez/tabuleiro/Peca.cs
+++ xadrez/tabuleiro/Peca.cs
@@ -17,5 +17,9 @@ namespace tabuleiro
             this.qteMovimentos = 0;
 
         }
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
